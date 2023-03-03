b221d4fbcaca576b236433bd452f525bce268b3e
Max Wright <47584716+blukoi@users.noreply.github.com>
2023-03-03 12:49:45 -0800

    Finished in-class exercise

--- Week07/inClassExercise.py
+++ Week07/inClassExercise.py
@@ -18,6 +18,7 @@ code1 = 1;
 code2 = 0;
 codemax = 3;
 code_timer = ticks_ms();
+count = 0;
 
 servo_obj = Servo(Pin(26)); # create a Servo object on G26 (yellow wire)
 
@@ -27,6 +28,7 @@ adc.atten(ADC.ATTN_11DB) # enable full-range on ADC pin
 
 while True:
     if state == "RED":
+        # print(state);
         for i in range(25):
             neopixel_strip[i] = (255,0,0);
             neopixel_strip.write();
@@ -35,18 +37,26 @@ while True:
             code2 += 1;
             print("Button Pressed");
             sleep(.1);
-    if state == "YELLOW":
+    elif state == "YELLOW":
+        # print(state)
         for i in range(25):
             neopixel_strip[i] = (255,255,0);
             neopixel_strip.write();
         if code1 > code2 and btn.value() == 0:
-            if ticks_ms() > btn_timer+100:
+            # print("Pressseeeeddddd");
+            count += 1;
+            print(count);
+            if count >= 15:
                 state = "GREEN";
                 code2 += 1;
-                btn_timer = ticks_ms();
                 print("Button Pressed a second time");
                 sleep(.1);
-    if state == "GREEN":
+        elif code1 > code2 and btn.value() == 1:
+            count = 0;
+    elif state == "GREEN":
+        for i in range(25):
+            neopixel_strip[i] = (0,255,0);
+            neopixel_strip.write();
         analog_val = adc.read(); # read analog value (0-1024)
         servo_val = map_value(analog_val, 0, 4095, 1000, 1500);
         servo_obj.write_us(servo_val);
@@ -55,3 +65,6 @@ while True:
     if code1 == codemax:
         code1 = 1;
         code2 = 0;
+    if (code1 == code2) and (ticks_ms() > code_timer+100):
+        code1 += 1;
+        code_timer = ticks_ms();
