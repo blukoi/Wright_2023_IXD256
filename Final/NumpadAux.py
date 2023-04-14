a83952134be344a994f2833b52e4ea4ca8590f3d
Max Wright <47584716+blukoi@users.noreply.github.com>
2023-04-14 13:51:05 -0700

    Fixed Bugs & Functionality
    Fixed all errors related to API requests; added potentiometer functionality to move between screens; fixed errors related to device resets; fixed errors related to text labels; started added IFTTT notification functionality.

--- Final/NumpadAux.py
+++ Final/NumpadAux.py
@@ -6,27 +6,39 @@ from m5stack_ui import *
 from uiflow import *
 import urequests
 import wifiCfg
-# import datetime
 import sys
-# import requests
 import json
-# import logging
+from m5mqtt import M5mqtt  # M5Stack MQTT library
 
-# GENERAL VARIABLES
-
+# SETUP
 screen = M5Screen()
 screen.clean_screen()
 screen.set_screen_bg_color(0x000)
-screenmode = "TEST"
-test = None
-testarea = "WORKING"
+screenmode = None
 
+# CONNECT TO WIFI
 # change arguments below to connect to the WiFi network, such as 'ACCD':
-wifiCfg.doConnect('ACCD', 'tink1930')
+wifiCfg.doConnect('Maximus Prime', 'maxisthebest')
 # check WiFi connection status:
 if wifiCfg.wlan_sta.isconnected():
     screen.set_screen_bg_color(0x00FF00)
-    print('connected to WiFi network..')
+
+# create the MQTT feed by inserting your Adafruit IO username and key:
+mqtt_feed = M5mqtt(
+    client_id = 'testclient',  # any name for your device (MQTT client)
+    server = 'io.adafruit.com',  # MQTT broker address
+    port = 1883,  # port to use for the connection
+    user = 'theblukoi',  # your Adafruit IO username 
+    password = 'aio_kpwm55RLdTB1wh14xjtJxQwqu3oF',  # your Adafruit IO key (check yellow key icon)
+    keepalive = 300  # keep alive timeout  
+)
+mqtt_feed.start()
+
+# BACKGROUNDS
+line0 = M5Line(x1=0, y1=0, x2=0, y2=0, color=0xd55b5b, width=0, parent=None)
+line1 = M5Line(x1=0, y1=0, x2=0, y2=0, color=0x639d67, width=0, parent=None)
+line2 = M5Line(x1=0, y1=0, x2=0, y2=0, color=0xf9ff64, width=0, parent=None)
+line3 = M5Line(x1=0, y1=0, x2=0, y2=0, color=0x3fbfed, width=0, parent=None)
 
 # SENSORS
 angle = unit.get(unit.ANGLE, (35,34));
@@ -39,94 +51,180 @@ def map_value(in_val, in_min, in_max, out_min, out_max):
         v = out_max;
     return int(v);
 
-# CLOCK VARIABLES
-hours = 14
-mins = 27
-secs = 0
-label0 = None
-label1 = None
-label2 = None
-label3 = None
-label4 = None
-currenttime = None
+# WORKING VARIABLES
+time = None
+timedata = None
+hour = None
+minute = None
+day = None
+month = None
+dayofweek = None
+weather = None
+weatherdata = None
+weathernow = None
+weathercondition = None
+lowdeg = None
+highdeg = None
+weathertoday = None
+timelabel = M5Label('', x=40, y=70, color=0x000000, font=FONT_MONT_48, parent=None)
+# M5Textarea(text='', x=0, y=0, w=None, h=None)
+# timelabel = M5Textarea()
 
-# DESTINY VARIABLES
-# 25 requests per second !!!
-HEADERS = {"X-API-Key":'5324217822ec4b6fbb6c9524520d15bb'}
-
-# username = 
-
-label5 = None
-label6 = None
-label7 = None
-
-# TEST
-touch_button0 = None
-# def touch_button0_pressed():
-#     # global params
-#     screen.set_screen_bg_color(0xff0000)
-#     pass
+# timelabel = M5Label('', x=40, y=82, color=0x000000, font=FONT_MONT_48, parent=None)
+daylabel = M5Label('', x=40, y=74, color=0x000000, font=FONT_MONT_22, parent=None)
+datelabel = M5Label('', x=40, y=104, color=0x000000, font=FONT_MONT_48, parent=None)
+currently = M5Label('', x=40, y=76, color=0x000000, font=FONT_MONT_22, parent=None)
+currentdegree = M5Label('', x=40, y=110, color=0x000000, font=FONT_MONT_48, parent=None)
+condition = M5Label('', x=40, y=164, color=0x000000, font=FONT_MONT_22, parent=None)
+low = M5Label('', x=40, y=100, color=0x000000, font=FONT_MONT_22, parent=None)
+high = M5Label('', x=160, y=100, color=0x000000, font=FONT_MONT_22, parent=None)
+lowdegree = M5Label('', x=40, y=130, color=0x000000, font=FONT_MONT_48, parent=None)
+highdegree = M5Label('', x=160, y=130, color=0x000000, font=FONT_MONT_48, parent=None)
+forecast = M5Label('', x=40, y=184, color=0x000000, font=FONT_MONT_22, parent=None)
 
 while True:
-    x = angle.read();
-    x_adj = map_value(x, in_min = 0, in_max = 1024, out_min = 1, out_max = 4);
-    y = hover.distance;
-    y_adj = map_value(y, in_min = 0, in_max = 8191, out_min = 1, out_max = 10);
-    '''if x_adj == 1:
-        screedmode = "CLOCK"
+    screen.set_screen_bg_color(0xFFFFFF)
+    x = angle.read()
+    x_adj = map_value(x, in_min = 0, in_max = 1024, out_min = 1, out_max = 4)
+    y = hover.distance
+    y_adj = map_value(y, in_min = 0, in_max = 8191, out_min = 1, out_max = 10)
+    if y_adj > 0:
+        mqtt_feed.publish(
+            'theblukoi/feeds/test', # path
+            str(y_adj) # turn variable into a string to publish
+        );
+        wait(1);
+    if x_adj == 1:
+        screenmode = 'CLOCK'
     if x_adj == 2:
-        screedmode = "DESTINY1"
+        screenmode = 'DATE'
     if x_adj == 3:
-        screedmode = "DESTINY2"
+        screenmode = 'WEATHER'
     if x_adj == 4:
-        screedmode = "DESTINY3"'''
-    if screenmode == "TEST":
+        screenmode = 'FORECAST'
+    '''if screenmode == "TEST":
         try:
             screen.clean_screen()
             screen.set_screen_bg_color(0xFF0000)
-            # touch_button0 = M5Btn(text='Change', x=112, y=198, w=96, h=42, bg_c=0xFFFFFF, text_c=0x000000, font=FONT_MONT_14, parent=None)
-            # touch_button0.pressed(touch_button0_pressed)
-            time = urequests.request(method='GET', url = "http://www.worldtimeapi.org/api/timezone/America/Los_Angeles")
-            currenttime = time.json()
-            # print(currenttime)
-            label0 = M5Label(currenttime['datetime'], x=26, y=96, color=0xFFFFFF, font=FONT_MONT_14, parent=None)
+            time = urequests.get(url = "http://www.worldtimeapi.org/api/timezone/America/Los_Angeles")
+            req_data = time.json()
+            datetime = req_data['datetime']
+            label0.set_text(datetime)
+            # label1.set_text('distance = ' + str(hover.distance))
             wait(1)
         except:
             screen.clean_screen()
             screen.set_screen_bg_color(0xFF00FF)
             wait(1)
-        '''try:
-            response = urequests.request(method='GET', url = "https://timeapi.io/api/Time/current/zone?timeZone=America/Los_Angeles");
-            screen.clean_screen()
-            screen.set_screen_bg_color(0xFF0000)
-        except:
-            screen.clean_screen()
-            screen.set_screen_bg_color(0x0000FF)
-        screen.clean_screen()
-        inventoryItem = r.json()
-        test = M5Label(inventoryItem['Response']['data']['inventoryItem']['itemName'], x=26, y=96, color=0xFFFFFF, font=FONT_MONT_48, parent=None)
-        print(inventoryItem['Response']['data']['inventoryItem']['itemName'])'''
+        wait(1)'''
+    if screenmode == 'CLOCK':
+        # screen.clean_screen()
+        screen.set_screen_bg_color(0xFFFFFF)
+        line0 = M5Line(x1=0, y1=90, x2=20, y2=90, color=0xd55b5b, width=180, parent=None)
+        line1 = M5Line(x1=0, y1=190, x2=20, y2=190, color=0x639d67, width=20, parent=None)
+        line2 = M5Line(x1=0, y1=210, x2=20, y2=210, color=0xf9ff64, width=20, parent=None)
+        line3 = M5Line(x1=0, y1=230, x2=20, y2=230, color=0x3fbfed, width=20, parent=None)
+
+        # Don't need these variables for this screen
+        datelabel.set_text('')
+        daylabel.set_text('')
+        currently.set_text('')
+        currentdegree.set_text('')
+        condition.set_text('')
+        low.set_text('')
+        high.set_text('')
+        lowdegree.set_text('')
+        highdegree.set_text('')
+        forecast.set_text('')
+
+        # Fill out this screen
+        time = urequests.get(url='https://timeapi.io/api/Time/current/zone?timeZone=America/Los_Angeles')
+        timedata = time.json()
+        hour = timedata['hour']
+        minute = timedata['minute']
+        timelabel.set_text(str(hour) + ':' + str(minute))
         wait(1)
-    if screenmode == "CLOCK":
-        if secs >= 59:
-            mins += 1
-            secs = -1
-            if mins >= 59:
-                hours += 1
-                mins = 0
-                if hours >= 24:
-                    mins = 0
-                    secs = 0
-                    hours = 0
-        wait(.9230)
-        screen.clean_screen()
-        secs += 1
-        label0 = M5Label(str(hours), x=26, y=96, color=0xFFFFFF, font=FONT_MONT_48, parent=None)
-        label1 = M5Label(':', x=103, y=96, color=0xFFFFFF, font=FONT_MONT_48, parent=None)
-        label2 = M5Label(str(mins), x=128, y=96, color=0xFFFFFF, font=FONT_MONT_48, parent=None)
-        label3 = M5Label(':', x=204, y=96, color=0xFFFFFF, font=FONT_MONT_48, parent=None)
-        label4 = M5Label(str(secs), x=231, y=96, color=0xFFFFFF, font=FONT_MONT_48, parent=None)
-    if screenmode == "CLOCK2":
-        screen.clean_screen()
-        # currenttime = datetime.time.now()
-        label0 = M5Label(str(currenttime), x=26, y=96, color=0xFFFFFF, font=FONT_MONT_48, parent=None)
+    if screenmode == 'DATE':
+        # screen.clean_screen()
+        screen.set_screen_bg_color(0xFFFFFF)
+        line0 = M5Line(x1=0, y1=10, x2=20, y2=10, color=0xd55b5b, width=20, parent=None)
+        line1 = M5Line(x1=0, y1=110, x2=20, y2=110, color=0x639d67, width=180, parent=None)
+        line2 = M5Line(x1=0, y1=210, x2=20, y2=210, color=0xf9ff64, width=20, parent=None)
+        line3 = M5Line(x1=0, y1=230, x2=20, y2=230, color=0x3fbfed, width=20, parent=None)
+
+        # Don't need these variables for this screen
+        timelabel.set_text('')
+        currently.set_text('')
+        currentdegree.set_text('')
+        condition.set_text('')
+        low.set_text('')
+        high.set_text('')
+        lowdegree.set_text('')
+        highdegree.set_text('')
+        forecast.set_text('')
+
+        # Fill out this screen
+        time = urequests.get(url='https://timeapi.io/api/Time/current/zone?timeZone=America/Los_Angeles')
+        timedata = time.json()
+        day = timedata['day']
+        month = timedata['month']
+        dayofweek = timedata['dayOfWeek']
+        datelabel.set_text(str(month) + '/' + str(day))
+        daylabel.set_text(str(dayofweek))
+        wait(1)
+    if screenmode == 'WEATHER':
+        # screen.clean_screen()
+        screen.set_screen_bg_color(0xFFFFFF)
+        line0 = M5Line(x1=0, y1=10, x2=20, y2=10, color=0xd55b5b, width=20, parent=None)
+        line1 = M5Line(x1=0, y1=30, x2=20, y2=30, color=0x639d67, width=20, parent=None)
+        line2 = M5Line(x1=0, y1=130, x2=20, y2=130, color=0xf9ff64, width=180, parent=None)
+        line3 = M5Line(x1=0, y1=230, x2=20, y2=230, color=0x3fbfed, width=20, parent=None)
+
+        # Don't need these variables for this screen
+        datelabel.set_text('')
+        daylabel.set_text('')
+        timelabel.set_text('')
+        low.set_text('')
+        high.set_text('')
+        lowdegree.set_text('')
+        highdegree.set_text('')
+        forecast.set_text('')
+
+        # Fill out this screen
+        weather = urequests.get(url='https://api.weatherapi.com/v1/current.json?key=f6fb81c6e7e5488081c173341231404&q=Los_Angeles&aqi=no')
+        weatherdata = weather.json()
+        weathernow = weatherdata['current']['temp_f']
+        weathercondition = weatherdata['current']['condition']['text']
+        currently.set_text('Currently')
+        currentdegree.set_text(str(weathernow) + '°')
+        condition.set_text(str(weathercondition))
+        # timelabel.set_text(str(weathernow))
+        wait(1)
+    if screenmode == 'FORECAST':
+        # screen.clean_screen()
+        screen.set_screen_bg_color(0xFFFFFF)
+        line0 = M5Line(x1=0, y1=10, x2=20, y2=10, color=0xd55b5b, width=20, parent=None)
+        line1 = M5Line(x1=0, y1=30, x2=20, y2=30, color=0x639d67, width=20, parent=None)
+        line2 = M5Line(x1=0, y1=50, x2=20, y2=50, color=0xf9ff64, width=20, parent=None)
+        line3 = M5Line(x1=0, y1=150, x2=20, y2=150, color=0x3fbfed, width=180, parent=None)
+
+        # Don't need these variables for this screen
+        datelabel.set_text('')
+        daylabel.set_text('')
+        timelabel.set_text('')
+        currently.set_text('')
+        currentdegree.set_text('')
+        condition.set_text('')
+
+        # Fill out this screen
+        weather = urequests.get(url='https://api.weatherapi.com/v1/forecast.json?key=f6fb81c6e7e5488081c173341231404&q=Los_Angeles&days=1&aqi=no&alerts=yes')
+        weatherdata = weather.json()
+        lowdeg = round(weatherdata['forecast']['forecastday'][0]['day']['mintemp_f'])
+        highdeg = round(weatherdata['forecast']['forecastday'][0]['day']['maxtemp_f'])
+        weathertoday = weatherdata['forecast']['forecastday'][0]['day']['condition']['text']
+        low.set_text('Low')
+        high.set_text('High')
+        lowdegree.set_text(str(lowdeg) + "°")
+        highdegree.set_text(str(highdeg) + "°")
+        forecast.set_text(weathertoday)
+        wait(1)
